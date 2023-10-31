db51f280141f84d869079349d36d27d985baeb60
Muhammad Ahmed Raza <razaahmed141@gmail.com>
2023-10-31 15:33:10 +0500

    updated nlp.py

--- nlp.py
+++ nlp.py
@@ -10,6 +10,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+# Define the pipeline globally
+pipeline = Pipeline([
+    ('vectorizer', TfidfVectorizer()),
+    ('classifier', LogisticRegression())
+])
+
 def preprocess_text(text: str) -> str:
     """
     Preprocesses the text by removing special characters, converting to lowercase,
@@ -24,9 +30,9 @@ def preprocess_text(text: str) -> str:
     lemma = WordNetLemmatizer()
     stop_words = set(stopwords.words('english'))
     
-    msgs = re.sub('[^a-zA-Z]', ' ', text).lower().split()
-    msgs = [lemma.lemmatize(word) for word in msgs if word not in stop_words]
-    msgs = ' '.join(msgs)
+    msgs = re.sub('[^a-zA-Z]', ' ', text).lower().split()  # removing special characters and converting to lowercase
+    msgs = [lemma.lemmatize(word) for word in msgs if word not in stop_words]  # lemmatizing the words and removing stopwords
+    msgs = ' '.join(msgs)  # joining the words again to form a sentence 
     
     return msgs
 
@@ -40,17 +46,12 @@ def train_and_evaluate_model(df: pd.DataFrame) -> float:
     Returns:
         float: The accuracy score of the model.
     """
-    df['Spam'] = pd.get_dummies(df['Category'], drop_first=True)
-    y = df['Spam']
-    messages = df['Message']
-    corpus = [preprocess_text(i) for i in messages]
+    df['Spam'] = pd.get_dummies(df['Category'], drop_first=True)  # encoding the target variable
+    y = df['Spam']  # target variable
+    messages = df['Message']  # feature / input variable
+    corpus = [preprocess_text(i) for i in messages]  # preprocessing the input text
     
-    pipeline = Pipeline([
-        ('vectorizer', TfidfVectorizer()),
-        ('classifier', LogisticRegression())
-    ])
-    
-    x_train, x_test, y_train, y_test = train_test_split(corpus, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(corpus, y, test_size=0.2, random_state=42)  # splitting the dataset into train and test
     pipeline.fit(x_train, y_train)
     pred = pipeline.predict(x_test)
     acc_score = accuracy_score(y_test, pred)
@@ -63,3 +64,12 @@ df = pd.read_csv('spam.csv')
 # Train and evaluate the model:
 accuracy = train_and_evaluate_model(df)
 print(f"Accuracy Score: {accuracy}")
+
+# Reading the User email as input:    
+user_input = input("Enter your email: ")
+user_email = preprocess_text(user_input)
+prediction = pipeline.predict([user_email])
+if prediction[0] == 0:
+    print("Your email is Not Spam")
+else:
+    print("Your email is Spam! Hope you don't get it")
